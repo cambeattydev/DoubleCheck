3c2ff36bc0f538e23f6f9bba222c1950c467bc2e
Cameron Beatty <beatty2121@gmail.com>
2015-04-19 22:24:30 -0500

    Changed uninstall in package.json

--- scripts/createFile.js
+++ scripts/createFile.js
@@ -1,7 +1,7 @@
 var fs = require('fs'),
     file = "doubleCheckDatabaseTest.db";
 var fd = fs.openSync(file, "w");
-var sqlite3 = require('sqlite3').verbose(),
+var sqlite3 = require('sqlite3'),
     db = new sqlite3.Database(file);
 console.log("fd: " + fd);
   db.run("create table users(id integer primary key not null, username varchar(20) not null,   array varchar(400) not null)", function (err){
